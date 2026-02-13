750ea51dc0302b3afc9724037a9a70850ad5b06e
RafaDalmagro <rafa.limadalmagro@gmail.com>
2026-02-13 08:23:11 -0300

    feat: instala e configura o prisma ORM.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-import fastify from 'fastify';
+import fastify from "fastify";
+import { env } from "./env";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../prisma/generated/prisma/client";
+
+const connectionString = `${env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
 export const app = fastify();
+
+
